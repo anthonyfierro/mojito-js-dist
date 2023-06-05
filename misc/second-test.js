4aa1a2d6ecacfeaa1b63e652843944b2d829f502
Anthony <anthonyfierro1@gmail.com>
2023-06-05 12:10:17 -0400

    latest version of mojito dist configured

--- misc/second-test.js
+++ misc/second-test.js
@@ -1,12 +1,12 @@
 Mojito.addTest({
-  id: "ex1",
-  name: "Test Generated from Split Test app",
+  id: "ex2",
+  name: "Test Generated from Split Test app2",
   sampleRate: 1,
   state: "live",
   trigger: function (test) {
     // Only activate and bucket users into the experiment if they come from Google
     // (Runs as soon as Mojito.addTest is called)
-    if (true) test.activate();
+    test.activate();
   },
   recipes: {
     0: {
@@ -17,11 +17,12 @@ Mojito.addTest({
       js: function () {
         // This code will run once the test activates & the user is bucketed
         const button = document.querySelector(".button-test");
-        console.log("runs");
 
         if (button) {
           button.style.backgroundColor = "blue";
         }
+
+        console.log("test2 ran!");
       },
     },
   },
